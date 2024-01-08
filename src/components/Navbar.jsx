8c931c02c39efa2fc412806c3bbe3c2bd200a22d
Ahmed <ahmedbahij0@gmail.com>
2024-01-08 18:12:28 +0100

    i make a lot of modifications haha

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+
+    <div className="navbar-container">
+        <div className="navbar">
+            <div className="logo">Be</div>
+
+            <div className="others">
+
+                <div className="mode">Mode</div>
+                <div className="links">
+                <p>Menu</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default Navbar
