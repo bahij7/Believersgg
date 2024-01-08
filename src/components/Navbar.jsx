1e1d3eb11384bf517943d918085bb8a54af3a65a
Ahmed <ahmedbahij0@gmail.com>
2024-01-08 18:37:54 +0100

    another commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons'
 import { faSun } from '@fortawesome/free-solid-svg-icons'
