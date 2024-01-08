54b2d1ecee04cf320ad57e23b2b7f5af549264ae
Ahmed <ahmedbahij0@gmail.com>
2024-01-08 23:17:19 +0100

    Night's Commits 2

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 function Intro() {
   return (
     <div className='intro'>
-        <img src="" alt="colorsPalette" />
+        <img src="public/images/1.png" alt="colorsPalette" />
+        <h1>Because if we <span>Believe</span>, we <span>achieve.</span></h1>
+        <Link to='/'>Check our latest news <i><FontAwesomeIcon icon={faArrowUp} className='arrow' /></i></Link>
     </div>
   )
 }
