10fb9b57812b10d7d87266deab423f9baf703ceb
Ahmed <ahmedbahij0@gmail.com>
2024-01-10 13:30:46 +0100

    22

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
-
+import myImage from '../1.png'
 
 function Intro() {
   return (
     <div className='intro'>
-        <img src='' alt="colorsPalette" />
+        <img src={myImage} alt="colorsPalette" />
         <h1>Because if we <span>Believe,</span> we <span>achieve.</span></h1>
         <Link to='/'>Check our latest news <i><FontAwesomeIcon icon={faArrowUp} className='arrow' /></i></Link>
     </div>
