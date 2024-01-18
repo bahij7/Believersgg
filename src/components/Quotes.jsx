521d28107387369837e63909d9bfe2e7fee52d06
Ahmed <ahmedbahij0@gmail.com>
2024-01-18 23:40:42 +0100

    muchas commits

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Quote from './Quote'
 
 
 function Quotes() {
     const QuoteArr = [
-        {author: 'Salim Gharib', quote:"“Team believers team s3eb tl9a b7alha, drari m9atlin w3ndhoum strict vision. l'mr7ala li dwzt m3ahoum kanet special, 3amra khdma, w'fnefs 3amra dehk. They will be international one day, Just BELIEVE !!”", role: "BELIEVERS's Former player"},
-        {author: 'Ahmed Bahij', quote:"“We'll keep fighting no matter what”", role:"BELIEVERS's Leader"},
-        {author: 'Adam Kaddouri', quote:"“Nadyin w safe”", role: 'CPZEROs Leader'},
-        {author: 'Wendy', quote:"“Lesgo”", role: "BELIEVERS's Manager"},
+        {author: 'Ahmed Bahij', quote:"“No matter what comes our way, we'll keep fighting 🇲🇦⭐. #weBelieve”", role:"BELIEVERS's Leader"},
+        {author: 'Salim Gharib', quote:"“Team believers team s3eb tl9a b7alha, drari m9atlin w3ndhoum strict vision. l'mr7ala li dwzt m3ahoum kanet special, 3amra khdma, w'fnefs lw9t 3amra dehk. They will be international one day, Just BELIEVE !!”", role: "BELIEVERS's Former player"},
+        {author: 'Adam Kaddouri', quote:"“Believers squad intégré bzf o khdama 3la rasha soit f botolat diyal clan soit f botolat berra lclan o hiya ghada f tri9 mzyana  bl3b li kadir o l impact diyal kol membre f squad, saraha kanchofha mn squadat li professionel fga3 les cotes. #WeAreCPZERO 🏆”", role: "CPZERO's Leader"},
     ]
+    const QuoteArr2 = [
+        {author: 'Wendy', quote:"“Lesgo”", role: "BELIEVERS's Manager"},
+        {author: 'Shoto', quote:"“A good team”", role: "ONETEAM's Co-Leader"},
+    ]
+
+    const [open, setOpen] = useState(false);
+    const handleQuotes = ()=>{
+        setOpen(!open)
+    }
   return (
         <div className='quotes'>
+           <div className="quotes-head">
+            <h2><span>Insights</span> from Those Who Walked the <span>Path With Us.</span></h2>
+           </div>
+
             {QuoteArr.map((quote)=> <Quote author={quote.author} role={quote.role} quote={quote.quote}/>)}
+            {open && QuoteArr2.map((quote)=> <Quote author={quote.author} role={quote.role} quote={quote.quote}/>)}
+            <div className="quote-btn">
+                <button onClick={handleQuotes}>{open? 'Hide All': 'See More'}</button>
+            </div>
+
         </div>
   )
 }
--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -7,7 +7,7 @@ function Second() {
   return (
     <div className='second'> 
       <iframe width="670" height="377" src="https://www.youtube.com/embed/Kf_YuFrHDTs" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <Link to='/Team'>Take a look into our squad <FontAwesomeIcon icon={faArrowUp} className='arrow' /></Link>
+        <Link to='/Team'>Take a deep look into our squad <FontAwesomeIcon icon={faArrowUp} className='arrow' /></Link>
     </div>
   )
 }
